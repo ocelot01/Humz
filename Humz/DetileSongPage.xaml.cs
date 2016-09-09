22d243893136cd68d6c3ee2a82a0a17b418748af
Supamard <kwan_st82@hotmail.com>
2016-09-09 11:17:17 +0700

    เพิ่มการแสดงผลจากหน้าค้นหาไปยังหน้ารายละเอียด

--- Humz/DetileSongPage.xaml.cs
+++ Humz/DetileSongPage.xaml.cs
@@ -7,9 +7,12 @@ namespace Humz
 {
 	public partial class DetileSongPage : ContentPage
 	{
-		public DetileSongPage()
+		public DetileSongPage(SongModel song)
 		{
 			InitializeComponent();
+
+			labelSongName.Text = "ชื่อเพลง: " + song.SongName;
+			labelSingeg.Text = "นักร้อง: " + song.Singer;
 		}
 
 	}
--- Humz/SearchListPage.xaml.cs
+++ Humz/SearchListPage.xaml.cs
@@ -7,15 +7,38 @@ namespace Humz
 {
 	public partial class SearchListPage : ContentPage
 	{
+		SongModel newSong = new SongModel();
+
 		public SearchListPage()
 		{
 			InitializeComponent();
+
+			newSong.SongName = "Down with the Sickness";
+			newSong.Singer = "Distrub";
+
+			labelSong.Text = labelSong.Text + newSong.SongName;
+			labelSinger.Text = labelSinger.Text + newSong.Singer;
 		}
 
-		//async void Handle_Clicked(object sender, System.EventArgs e)
-		//{
-			
-		//}
+		async void Handle_Clicked(object sender, System.EventArgs e)
+		{
+			//var songList = new SongModel[4];
+
+			//for (int i = 0; i < 4; i++)
+			//{
+			//	SongModel newSong = new SongModel();
+			//	newSong.SongName = "songNameMock" + i.ToString();
+			//	newSong.Singer = "Cold Play";
+
+			//	songList.SetValue(newSong, i);
+			//}
+
+			SongModel cacheSong = new SongModel();
+			cacheSong.SongName = labelSong.Text;
+			cacheSong.Singer = labelSinger.Text;
+
+			await Navigation.PushAsync(new DetileSongPage(newSong), true);
+		}
 	}
 }
 
