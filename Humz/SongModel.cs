11915e2411bfda5af72053b19dc38d44112f7b42
Supamard <kwan_st82@hotmail.com>
2016-09-09 10:12:52 +0700

    Create new class for song

---
+++ Humz/SongModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Humz
+{
+	public class SongModel
+	{
+		public SongModel()
+		{
+		}
+
+		public string SongName
+		{
+			get;
+			set;
+		}
+
+		public string Singer
+		{
+			get;
+			set;
+		}
+	}
+}
+
